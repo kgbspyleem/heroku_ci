65fd45c12b158e32e3b01573653a3264c1e7150d
Test1 <kgbspy@hanmail.net>
2018-06-26 17:05:52 +0900

    1

--- src/main/java/com/i2max/bootweb/controller/DemoApplication.java
+++ src/main/java/com/i2max/bootweb/controller/DemoApplication.java
@@ -4,12 +4,32 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;  
-import org.springframework.context.annotation.ComponentScan;  
+import org.springframework.context.annotation.ComponentScan; 
+import org.springframework.web.bind.annotation.*;
  
-@SpringBootApplication  
+
+import org.springframework.stereotype.*;
+ 
+ @SpringBootApplication 
+@ComponentScan(basePackages = "com.i2max.bootweb") 
 public class DemoApplication { 
 
-	private final Logger logger = LoggerFactory.getLogger(this.getClass()); 
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @RequestMapping("/test")
+    @ResponseBody
+    String home() { 
+      return "Hello World!";
+    }
+    
+    
+
+    @RequestMapping("/test1")
+    @ResponseBody
+    String home1() { 
+      return "Hello Worldsddasasd!";
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
         
