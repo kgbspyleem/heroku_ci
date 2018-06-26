2548422152bb47b6b40d82040b0c5c867382be60
Test1 <kgbspy@hanmail.net>
2018-06-26 15:11:50 +0900

    1

--- src/main/java/com/i2max/security/WebSecurityConfig.java
+++ src/main/java/com/i2max/security/WebSecurityConfig.java
@@ -4,35 +4,23 @@ package com.i2max.security;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
  
-@Configuration
-@EnableWebSecurity
+@Configuration 
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
   
 	private static final Logger logger = LoggerFactory.getLogger(WebSecurityConfig.class);
-
+	 
     @Override
     protected void configure(final HttpSecurity http) throws Exception { 
-    	   http 
-           .authorizeRequests()
-           .antMatchers(
-                   "/favicon.ico",
-                   "/404.html",
-                   "/403.html",
-                   "/500.html",
-                   "/sfdcauth/**",
-                   "/login",
-                   "/logout",
-                   "/password",
-                   "/images/**",
-                   "/css/**",
-                   "/fonts/**",
-                   "/icons/**")
-           .permitAll();
+        http
+    	.authorizeRequests()
+		.antMatchers("/**")
+		.permitAll();
        
     }
 }
