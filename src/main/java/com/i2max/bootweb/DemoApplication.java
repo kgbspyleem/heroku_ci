b29866e8ef1f63e96575f1d2e77fc42034a2de25
Test1 <kgbspy@hanmail.net>
2018-06-26 12:40:50 +0900

    1

--- src/main/java/com/i2max/bootweb/DemoApplication.java
+++ src/main/java/com/i2max/bootweb/DemoApplication.java
@@ -1,28 +1,35 @@
 package com.i2max.bootweb;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.autoconfigure.mustache.MustacheEnvironmentCollector;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
+ 
+
 import org.springframework.stereotype.*;
  
 
 @Controller
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication { 
+
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @RequestMapping("/")
     @ResponseBody
-    String home() {
-  
-      
-      
+    String home() { 
       return "Hello World!";
     }
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+        
     }
+     
 }
--- src/main/java/com/i2max/security/WebSecurityConfig.java
+++ src/main/java/com/i2max/security/WebSecurityConfig.java
@@ -1,26 +1,9 @@
-/** ========================================================================= *
- * Copyright (C)  2017, 2018 Salesforce Inc ( http://www.salesforce.com/      *
- *                            All rights reserved.                            *
- *                                                                            *
- *  @author     Stephan H. Wissel (stw) <swissel@salesforce.com>              *
- *                                       @notessensei                         *
- * @version     1.0                                                           *
- * ========================================================================== *
- *                                                                            *
- * Licensed under the  Apache License, Version 2.0  (the "License").  You may *
- * not use this file except in compliance with the License.  You may obtain a *
- * copy of the License at <http://www.apache.org/licenses/LICENSE-2.0>.       *
- *                                                                            *
- * Unless  required  by applicable  law or  agreed  to  in writing,  software *
- * distributed under the License is distributed on an  "AS IS" BASIS, WITHOUT *
- * WARRANTIES OR  CONDITIONS OF ANY KIND, either express or implied.  See the *
- * License for the  specific language  governing permissions  and limitations *
- * under the License.                                                         *
- *                                                                            *
- * ========================================================================== *
- */
+ 
 package com.i2max.security;
 
+ 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -30,9 +13,13 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+  
+	private static final Logger logger = LoggerFactory.getLogger(WebSecurityConfig.class);
+
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
-
+    	
+ 
         http
                 /* Allow the app to show in a frame */
                 .headers().frameOptions().disable()
