14cd3b64d2d57c3d0bde1b365c54f46571de4d5c
Test1 <kgbspy@hanmail.net>
2018-06-26 14:00:32 +0900

    1

--- src/main/java/com/i2max/security/WebSecurityConfig.java
+++ src/main/java/com/i2max/security/WebSecurityConfig.java
@@ -9,7 +9,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
  
- 
+@Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
   
