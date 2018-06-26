cdc2f5e9b0e8980fb6b43dc526a073003b8257ca
Test1 <kgbspy@hanmail.net>
2018-06-26 13:35:14 +0900

    1

--- src/main/java/com/i2max/security/WebSecurityConfig.java
+++ src/main/java/com/i2max/security/WebSecurityConfig.java
@@ -9,8 +9,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-
-@Configuration
+ 
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
   
@@ -22,6 +21,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     	logger.info("====================================== TEST");
     	http.authorizeRequests()
 		.antMatchers("/admin/**").hasRole("ADMIN")
-		.antMatchers("/").permitAll();
+		.antMatchers("/**").permitAll();
     }
 }
