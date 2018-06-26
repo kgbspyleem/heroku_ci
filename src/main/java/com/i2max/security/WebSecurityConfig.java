83ccc2935ac6f56d20473a4a0b691d4038de15c7
Test1 <kgbspy@hanmail.net>
2018-06-26 15:59:50 +0900

    1

--- src/main/java/com/i2max/security/WebSecurityConfig.java
+++ src/main/java/com/i2max/security/WebSecurityConfig.java
@@ -10,11 +10,15 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
  
-@Configuration 
+@Configuration
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
   
-	private static final Logger logger = LoggerFactory.getLogger(WebSecurityConfig.class);
-	 
+	private static final Logger logger = LoggerFactory.getLogger(WebSecurityConfig.class); 
+	 public WebSecurityConfig()
+	 {
+		 logger.info("============================================== WebSecurityConfig Constructor");
+	 }
     @Override
     protected void configure(final HttpSecurity http) throws Exception { 
         http
