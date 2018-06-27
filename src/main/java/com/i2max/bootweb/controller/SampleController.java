6bccf4b2a2e9f9dd16645c4b9395808d7800b76d
Test1 <kgbspy@hanmail.net>
2018-06-27 14:40:07 +0900

    1

--- src/main/java/com/i2max/bootweb/controller/SampleController.java
+++ src/main/java/com/i2max/bootweb/controller/SampleController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class SampleController {
-	  @GetMapping("/sample")
+	    @GetMapping("/sample")
 	    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
 	        model.addAttribute("name", name);
 	        return "sample";
