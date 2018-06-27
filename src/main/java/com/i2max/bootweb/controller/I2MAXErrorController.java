6c5c846b3cf6420ded68ff3c02866713183d6bd7
Test1 <kgbspy@hanmail.net>
2018-06-27 14:43:32 +0900

    1

--- src/main/java/com/i2max/bootweb/controller/I2MAXErrorController.java
+++ src/main/java/com/i2max/bootweb/controller/I2MAXErrorController.java
@@ -5,8 +5,10 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class I2MAXErrorController implements ErrorController  {
  
 	    @Override
