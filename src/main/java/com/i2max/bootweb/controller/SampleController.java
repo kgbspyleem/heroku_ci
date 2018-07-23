1e9e1b536b23db117cf5d014082f860710a94e42
Test1 <kgbspy@hanmail.net>
2018-07-23 16:54:45 +0900

    1

--- src/main/java/com/i2max/bootweb/controller/SampleController.java
+++ src/main/java/com/i2max/bootweb/controller/SampleController.java
@@ -32,5 +32,33 @@ public class SampleController {
 	    }
 	    
 	    
+        @RequestMapping(value = "/mc_test/save", method = RequestMethod.POST)
+  	    @ResponseBody
+  	    public String mc_save(@RequestParam(value="encodedJWT ", defaultValue="") String encodedJWT ,@RequestParam(value="secret", defaultValue="") String secret  ) {   
+          	logger.info("encodedJWT secret =================================" + encodedJWT + "=============" + secret); 
+  	        return "save World!";
+  	    }
+  	    
+        @RequestMapping(value = "/mc_test/publish", method = RequestMethod.POST)
+  	    @ResponseBody
+  	    public String mc_publish(@RequestParam(value="encodedJWT ", defaultValue="") String encodedJWT ,@RequestParam(value="secret", defaultValue="") String secret  ) {   
+          	logger.info("encodedJWT secret =================================" + encodedJWT + "=============" + secret); 
+  	        return "publish World!";
+  	    }
+  	    
+        @RequestMapping(value = "/mc_test/validate", method = RequestMethod.POST)
+  	    @ResponseBody
+  	    public String mc_validate(@RequestParam(value="encodedJWT ", defaultValue="") String encodedJWT ,@RequestParam(value="secret", defaultValue="") String secret  ) {   
+          	logger.info("encodedJWT secret =================================" + encodedJWT + "=============" + secret); 
+  	        return "validate World!";
+  	    }
+  	    
+        @RequestMapping(value = "/mc_test/stop", method = RequestMethod.POST)
+  	    @ResponseBody
+  	    public String mc_stop(@RequestParam(value="encodedJWT ", defaultValue="") String encodedJWT ,@RequestParam(value="secret", defaultValue="") String secret  ) {   
+          	logger.info("encodedJWT secret =================================" + encodedJWT + "=============" + secret); 
+  	        return "stop World!";
+  	    }
+  	    
 	    
 }
