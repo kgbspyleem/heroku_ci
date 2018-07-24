a3217faf3e6386fef5f5a7d7e553127c90764054
Test1 <kgbspy@hanmail.net>
2018-07-24 13:14:13 +0900

    1

--- src/main/java/com/i2max/bootweb/controller/SampleController.java
+++ src/main/java/com/i2max/bootweb/controller/SampleController.java
@@ -4,34 +4,36 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class SampleController {
 	    private static final Logger logger = LoggerFactory.getLogger(SampleController.class);  
+	    
 	    @GetMapping("/sample")
 	    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
 	        model.addAttribute("name", name);
 	        return "sample";
 	    } 
 	    
-	    @RequestMapping("/hello")
+	    @GetMapping("/hello")
 	    public String helloJSP(Model model ) { 
 	    	model.addAttribute("name", "SpringBlog from Millky");
 	        return "hello";
 	    } 
 	   
-	     @RequestMapping(value = "/mc_test/test", method = RequestMethod.GET)
-		    @ResponseBody
-		    public String test(@RequestParam(value="encodedJWT ", defaultValue="") String encodedJWT ,@RequestParam(value="secret", defaultValue="") String secret  ) {   
+	    @PostMapping(value = "/mc_test/test") 
+        public String test(@RequestParam(value="encodedJWT ", defaultValue="") String encodedJWT ,@RequestParam(value="secret", defaultValue="") String secret  ) {   
 	        	logger.info("encodedJWT secret =================================" + encodedJWT + "=============" + secret); 
 		        return "Hello test!";
 		    }
 	    
-        @RequestMapping(value = "/mc_test/execute", method = RequestMethod.POST)
+	    @PostMapping(value = "/mc_test/execute")
 	    @ResponseBody
 	    public String execute(@RequestParam(value="encodedJWT ", defaultValue="") String encodedJWT ,@RequestParam(value="secret", defaultValue="") String secret  ) {   
         	logger.info("encodedJWT secret =================================" + encodedJWT + "=============" + secret); 
@@ -39,28 +41,28 @@ public class SampleController {
 	    }
 	    
 	    
-        @RequestMapping(value = "/mc_test/save" , method = RequestMethod.POST)
+	    @PostMapping(value = "/mc_test/save" )
   	    @ResponseBody
   	    public String mc_save( ) {   
        //   	logger.info("encodedJWT secret =================================" + encodedJWT + "=============" + secret); 
   	        return "(\"status\" : \"ok\"}";
   	    }
   	    
-        @RequestMapping(value = "/mc_test/publish" , method = RequestMethod.POST)
+	    @PostMapping(value = "/mc_test/publish")
   	    @ResponseBody
   	    public String mc_publish(@RequestParam(value="encodedJWT ", defaultValue="") String encodedJWT ,@RequestParam(value="secret", defaultValue="") String secret  ) {   
           	logger.info("encodedJWT secret =================================" + encodedJWT + "=============" + secret); 
   	        return "(\"status\" : \"ok\"}";
   	    }
   	    
-        @RequestMapping(value = "/mc_test/validate", method = RequestMethod.POST)
+	    @PostMapping(value = "/mc_test/validate")
   	    @ResponseBody
   	    public String mc_validate(@RequestParam(value="encodedJWT ", defaultValue="") String encodedJWT ,@RequestParam(value="secret", defaultValue="") String secret  ) {   
           	logger.info("encodedJWT secret =================================" + encodedJWT + "=============" + secret); 
   	        return "(\"status\" : \"ok\"}";
   	    }
   	    
-        @RequestMapping(value = "/mc_test/stop", method = RequestMethod.POST)
+	    @PostMapping(value = "/mc_test/stop")
   	    @ResponseBody
   	    public String mc_stop(@RequestParam(value="encodedJWT ", defaultValue="") String encodedJWT ,@RequestParam(value="secret", defaultValue="") String secret  ) {   
           	logger.info("encodedJWT secret =================================" + encodedJWT + "=============" + secret); 
