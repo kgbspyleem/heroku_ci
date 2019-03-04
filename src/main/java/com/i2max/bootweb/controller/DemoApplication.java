2ef7faff86f37067689685c8991c39a56e27dcc5
kgbspy <kgbspy@hanmail.net>
2019-03-04 14:01:00 +0900

    1

--- src/main/java/com/i2max/bootweb/controller/DemoApplication.java
+++ src/main/java/com/i2max/bootweb/controller/DemoApplication.java
@@ -7,9 +7,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan; 
-import org.springframework.web.bind.annotation.*;
- 
-
+import org.springframework.web.bind.annotation.*; 
 import org.springframework.stereotype.*;
  
 
